b6522fa77bcff40d767d560da6f5ec4c376f32f9
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-08-12 17:14:27 +0100

    Start sending frame timestamps across to the Vicon visualiser.

--- smg/vicon/live_vicon_interface.py
+++ smg/vicon/live_vicon_interface.py
@@ -11,7 +11,7 @@ class LiveViconInterface(ViconInterface):
 
     # CONSTRUCTOR
 
-    def __init__(self, host: str = "192.168.137.1:801"):
+    def __init__(self, host: str = "169.254.226.243:801"):
         """
         Construct a live Vicon interface.
 
