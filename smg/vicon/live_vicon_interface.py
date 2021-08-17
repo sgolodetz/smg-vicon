17a6bb8fd81022cda940e59b50c16e390c763158
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-08-17 09:34:01 +0100

    Update a comment.

--- smg/vicon/live_vicon_interface.py
+++ smg/vicon/live_vicon_interface.py
@@ -17,9 +17,8 @@ class LiveViconInterface(ViconInterface):
 
         .. note::
             In the Wytham Flight Lab, the host seems to be "169.254.226.243:801" when connecting via Ethernet,
-            and "192.168.137.1:801" when connecting via the WiFi hotspot. I've set the default to the WiFi one,
-            since that's most useful on a day-to-day basis. However, the Ethernet one may also sometimes be
-            needed, such as when only one WiFi is available and it's being used to connect to a drone.
+            and "192.168.137.1:801" when connecting via the WiFi hotspot. I've set the default to the Ethernet
+            one, since I only have one WiFi dongle and I need to connect to both the Vicon and the drone.
 
         :param host:    The host (IP address:port) on which the Vicon software is running.
         """
